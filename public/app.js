0743665f3f8dd828df17cf9c9b158e28016a2bd2
Jayson Pack <jayson.pack@mdu.com>
2014-07-03 10:28:00 -0600

    It works! Note: need to update host in angular for websockets to work.

--- public/app.js
+++ public/app.js
@@ -1,31 +1,49 @@
 var nodes = angular.module('nodes', []);
-var size = 10;
+nodes.controller('nodesCtrl', ['$scope', function ($scope) {
+    /*
+        Controller vars.
+     */
 
-nodes.controller('boardCtrl', ['$scope', function ($scope) {
-    /* Init */
+    var size = 10;
+    var i, j;
 
+    /*
+        Scope vars.
+     */
 
     // Set up the board.
     $scope.board = [];
-    for ( var i = 0; i < size; i++) {
+    for (i = 0; i < size; i++) {
         $scope.board[i] = [];
 
-        for ( var j = 0; j < size; j++) {
+        for (j = 0; j < size; j++) {
             $scope.board[i][j] = false;
         }
     }
 
-    /* Create our Web Socket var and register our events */
-    var socket = io.connect('http://localhost:8080');
+    // Set up the target
+    $scope.target = [];
+    for (i = 0; i < size; i++) {
+        $scope.target[i] = [];
 
-    socket.on('board-update', function (data) {
-        console.log("Board update recieved with data: " + data.row + " " + data.col + " " + data.tileValue);
+        for (j = 0; j < size; j++) {
+            // Set the value to a random true or false.
+            $scope.target[i][j] = Math.random()<.5;
+        }
+    }
 
-        $scope.board[data.row][data.col] = data.tileValue;
+    // Set up timer.
+    $scope.timer = 0;
+
+    /*
+     Scope functions
+     */
+
+    $scope.updateTimer = function(timer){
+        $scope.timer = timer;
         $scope.$apply();
-    });
+    };
 
-    /* Create scope functions */
     $scope.updateTile = function(row, col){
         console.log('Sending tile update: ' + row + ' ' + col);
 
@@ -33,19 +51,6 @@ nodes.controller('boardCtrl', ['$scope', function ($scope) {
 
         socket.emit('board-update', {'row': row, 'col': col, 'tileValue': $scope.board[row][col]});
     };
-}]);
-
-nodes.controller('targetCtrl', ['$scope', function($scope){
-    // Set up the target
-    $scope.target = [];
-    for ( var i = 0; i < size; i++) {
-        $scope.target[i] = [];
-
-        for (var j = 0; j < size; j++) {
-            // Set the value to a random true or false.
-            $scope.target[i][j] = Math.random()<.5;
-        }
-    }
 
     $scope.generateTarget = function(){
         for (i = 0; i < size; i++) {
@@ -54,5 +59,63 @@ nodes.controller('targetCtrl', ['$scope', function($scope){
                 $scope.target[i][j] = Math.random()<.5;
             }
         }
-    }
+    };
+
+    $scope.resetBoard = function () {
+        for(i = 0; i < size; i++){
+            for(j = 0; j < size; j++){
+                $scope.board[i][j] = false;
+            }
+        }
+    };
+
+    $scope.gameWin = function(){
+        alert('You win!');
+        $scope.resetBoard();
+        $scope.generateTarget();
+    };
+
+    $scope.gameLoss = function(){
+        alert('You Lose!');
+        $scope.resetBoard();
+        $scope.generateTarget();
+    };
+
+    /*
+        Create our Web Socket var and register our events
+    */
+    var socket = io.connect('http://162.57.48.18:8080');
+
+    socket.on('board-update', function (data) {
+        console.log("Board update recieved with data: " + data.row + " " + data.col + " " + data.tileValue);
+
+        $scope.board[data.row][data.col] = data.tileValue;
+        $scope.$apply();
+    });
+
+    socket.on('timer-update', function(timer){
+        $scope.updateTimer(timer);
+    });
+
+    socket.on('round-end', function(){
+        var score = 0;
+
+        for(i = 0; i < size; i++){
+            for(j = 0; j < size; j++){
+                if($scope.board[i][j] == $scope.target[i][j]){
+                    score++;
+                }
+            }
+        }
+
+        socket.emit('send-score', score);
+    });
+
+    socket.on('round-end-win', function(){
+        $scope.gameWin();
+    });
+
+    socket.on('round-end-lose', function(){
+        $scope.gameLoss();
+    });
 }]);
--- server.js
+++ server.js
@@ -20,15 +20,74 @@ app.get('*', function(req, res) {
 
 
 // listen (start app with node server.js) ======================================
+var numConnections = 0;
+var reportedScores;
+var winner = {'socket': null, 'score': 0};
+
 io.on('connection', function(socket){
 	console.log('Connection established, socket id ' + socket.id);
+    numConnections++;
 
     socket.on('board-update', function(data){
         console.log('board-update recieved with data: ' + data.row + " " + data.col + " " + data.tileValue);
         console.log('broadcasting new board info to all clients from ' + socket.id);
         socket.broadcast.emit('board-update', data);
     });
+
+    socket.on('send-score', function(score){
+        reportedScores++;
+
+        if(score > winner.score){
+            winner.socket = socket;
+            winner.score = score;
+        }
+    });
+
+    socket.on('disconnect', function(){
+        numConnections--;
+    });
 });
+
+
+// Rounds.
+var timer = 60;
+setInterval(function(){
+    timer--;
+
+    io.emit('timer-update', timer);
+
+    if(timer == 0) {
+        console.log("Round end.");
+
+        timer = 60;
+
+        reportedScores = 0;
+        winner.score = -1;
+
+        var waitInt;
+
+        io.emit('round-end');
+
+        var waitForScores = function () {
+            console.log("Waiting for scores... (" + reportedScores + "/" + numConnections + ")");
+
+            if (reportedScores == numConnections) {
+                console.log("All scores received! Sending win/loss messages.");
+
+                winner.socket.emit('round-end-win');
+                winner.socket.broadcast.emit('round-end-lose');
+
+                clearInterval(waitInt);
+
+                return true;
+            } else {
+                return false;
+            }
+        };
+
+        waitInt = setInterval(waitForScores, 1000);
+    }
+}, 1000);
 
 server.listen(8080);
 console.log("App listening on port 8080");
